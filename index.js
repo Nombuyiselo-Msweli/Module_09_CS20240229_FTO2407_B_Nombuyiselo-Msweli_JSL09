3d7b8ed1bb75a22a1cde3a6f8bb50d7442328db0
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-09-29 09:42:49 +0200

    CORRECTED ERROR - updated index.js with data.urls.full, was reading as null because I typed data.url.full instead of data.urls.full. The image is showing up now

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(response => response.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.url.full})`
+        document.body.style.backgroundImage = `url(${data.urls.full})`
     })
+
