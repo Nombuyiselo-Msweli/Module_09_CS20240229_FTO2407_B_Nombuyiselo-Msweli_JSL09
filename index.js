503cd615e5d70bdb9fb07d9a6bbcb76efc0bd75d
Nombuyiselo Msweli <mswelinombuyiselo@gmail.com>
2024-09-29 13:07:53 +0200

    updated index.js with fethc API to get the image and name of the dogecoin crypotcurrency

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.getElementById("photographer").textContent = `By : ${data.user.name}`
     })
 
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        document.getElementById("coin-image").src = data.image.small
+        document.getElementById("coin-name").innerHTML = data.name
+    })
+    .catch(err => console.error(err))
+    
